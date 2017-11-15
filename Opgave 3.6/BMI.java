dda93ab2d3fc893a31c2edebb687f656842b55aa
Mikkel Sørensen <Mikk4211@edu.easj.dk>
2017-11-15 07:44:54 +0100

    Update BMI.java

--- Opgave 3.6/BMI.java
+++ Opgave 3.6/BMI.java
@@ -1,22 +1,30 @@
+/* Lavet af Mikkel Sørensen
+Mikk4211@edu.easj.dk
+EASJ Næstved, DAT 1 */
+
 package MikkelSorensen;
 
 import java.util.Scanner;
 
 public class BMI {
     public static void main(String[] args) {
-
+        
+        //To scanner objects, til indtasning af højde og vægt.
         Scanner højde = new Scanner(System.in);
         Scanner vægt = new Scanner(System.in);
-
+        
+        //Her bliver brugeren bedt om at indtaste data
         System.out.println("Insert height in metres and cm, with a comma seperating them: ");
         double h = højde.nextDouble();
         System.out.println("Insert weight in kg: ");
         double v = vægt.nextDouble();
-
+        
+        //Beregning
         double BMI;
         BMI = (v/(h*h));
         System.out.println("Your BMI is: " + BMI);
 
+        //Behandling af indtastet data (Hvad er dit BMI)
         if (BMI <= 18.5) {
             System.out.println("You are underweight");
         } else if (BMI <= 25) {
